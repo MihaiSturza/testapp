73c441cc4d0d5ade52e191a65b51e44029e176d4
Mihai Sturza <sturzamimi@gmail.com>
2020-03-24 18:03:46 +0200

    installed mysql

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
 
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "pushtoken.cxy1a4hn8hof.eu-central-1.rds.amazonaws.com",
+  user: "mimi",
+  password: "Odxll9QBlWaBV092cgEg",
+  database: "tokens"
+});
+
+connection.connect();
+
 app.get("/", (req, res) => {
-  res.send("Hello World, nice to see the change");
+  connection.query("SELECT * FROM `token`", (err, res, fields) => {
+    res.send(`Error: ${err}, res: ${res}, fields: ${fields}`);
+  });
 });
 
 app.listen(5000);
