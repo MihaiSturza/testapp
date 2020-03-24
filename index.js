5703f140e87fb23123e34befdce6d4ba77763f5b
Mihai Sturza <sturzamimi@gmail.com>
2020-03-24 10:29:00 +0200

    Minor change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World, nice to see the change");
 });
 
 app.listen(5000);
